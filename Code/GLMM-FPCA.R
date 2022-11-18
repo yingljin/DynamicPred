0d966a69ef810ccb83bc5c5e1879dd338cd5ff62
Ying Jin <ying.jin@cuanschutz.edu>
2022-11-18 11:54:48 -0700

    add in-sample prediction on binned data

--- Code/GLMM-FPCA.R
+++ Code/GLMM-FPCA.R
@@ -45,8 +45,52 @@ mat_pred_unique <- matrix(df_pred_unique$eta_hat, nrow=N, ncol=n_bin, byrow = T)
 # dim(mat_pred_unique)
 
 # fPCA
-fpca_fit <- fpca.face(mat_pred_unique, pve = 0.95, argvals = unique(df_pred_unique$bin), knots=20)
+fpca_fit <- fpca.face(mat_pred_unique, pve = 0.95, argvals = unique(df_pred_unique$bin), knots=20, var=T)
 # fpca_fit <- face.sparse(df_pred_unique, argvals.new = 1:J, knots = 20, pve=0.95)
+
+# Things for prediction
+tao <- diag(fpca_fit$evalues) ## egienvalues, also var-cov matrix of scores
+allB <- fpca_fit$efunctions ## eigen functions on the entire binned grid
+fpca_fit$mu ## mean function
+fpca_fit$sigma2 ## residual variance
+
+
+
+##### In-sample prediction on binned grid #####
+
+# start with one obesrvation
+df_id1 <- df_pred_latent %>% filter(id==1 & sind_inx <= 300) %>% 
+  select(id, bin, eta_hat) %>% distinct(.)
+
+obs_bin <- length(unique(df_id1$bin))
+
+B <- allB[1:obs_bin, ] # eigenfunctions up to observed time point
+
+# score
+score_hat <- tao %*% t(B) %*% solve(B%*%tao%*%t(B)+fpca_fit$sigma2*diag(obs_bin)) %*% (df_id1$eta_hat-fpca_fit$mu[1:obs_bin])
+
+fpca_fit$scores[1, ] ## scores from fPCA face
+
+# prediction
+df_pred_id1 <- df_pred_latent %>% filter(id==1 )
+
+eta_pred <- fpca_fit$mu+allB%*%score_hat
+
+df_pred_latent %>%
+  filter(id==1) %>% 
+  select(id, bin, eta_hat) %>% distinct(.) %>% 
+  mutate(eta_pred=eta_pred) %>%
+  ggplot()+
+   geom_line(aes(x=bin, y = eta_hat))+
+   geom_line(aes(x=bin, y = eta_pred), col="red")
+
+
+
+
+
+
+
+
 
 
 #### Project eigen functions to a denser grid #####
@@ -57,30 +101,6 @@ dense_efunc <- matrix(NA, nrow = J, ncol = keigen)
 for(i in 1:keigen){
   dense_efunc[, i] <- approx(x=mid, y=fpca_fit$efunctions[ , i], xout=1:J)$y
 }
-
-
-
-##### Prediction #####
-
-# data with partically observed curves
-# observations up to 1
-df1 <- df %>% filter(id==1 & sind_inx<=100)
-
-# ggplot(df1)+
-#   geom_line(aes(x=sind_inx, y = eta_i, col = "red"))+
-#   geom_point(aes(x=sind_inx, y = Y, col = "blue"), size = 0.01)+
-#   xlim(1, J)
-
-
-# score with empirical bayes
-# fpca_fit$scores[1, ] ## scores from fPCA face
-# 
-# tao <- diag(fpca_fit$evalues) ## egenvalues, also var-cov matrix of scores
-# B <- dense_efunc[1:100, ]
-# 
-# uhat <- tao %*% t(B) %*% solve(B %*% tao %*% t(B)) %*% df1$Y
-
-
 
 
 
