90b11045ade2084fc1ed1066116f9708cbf3b0d3
Ying Jin <ying.jin@cuanschutz.edu>
2022-12-22 13:12:37 -0700

    start on out-of-sample pred

--- Code/DynPred.R
+++ Code/DynPred.R
@@ -18,19 +18,24 @@ in_samp_dyn_pred <- function(fpca_fit, tm=20, df_pred_unique, subj=1){
   partial_df <- df_pred_unique %>% filter(bin <= max_t & id == subj) 
   
   # re-estimate score
-  score_hat <- tao %*% t(Phi) %*% solve(Phi%*%tao%*%t(Phi)+fpca_fit$sigma2*diag(tm)) %*% (partial_df$eta_hat-fpca_fit$mu[1:tm])
+  H <-  tao %*% t(Phi) %*% solve(Phi%*%tao%*%t(Phi)+fpca_fit$sigma2*diag(tm))
+  score_hat <- H %*% (partial_df$eta_hat-fpca_fit$mu[1:tm])
   
   # predicte latent function
   eta_pred <- fpca_fit$mu+fpca_fit$efunctions%*%score_hat
   
-  return(eta_pred)
+  # predict std
+  eta_pred_var <- fpca_fit$efunctions %*% (tao - H %*% Phi %*% tao) %*% t(fpca_fit$efunctions) + fpca_fit$sigma2
+  
+  return(list(eta_pred = eta_pred, eta_pred_var = eta_pred_var,
+              eta_std_var = sqrt(diag(eta_pred_var))))
 }
 
 
-# in_samp_dyn_pred(fpca_fit, 60,df_pred_unique, 7)
-
-
-
-
-
-
+# try <- in_samp_dyn_pred(fpca_fit, 60, df_pred_unique, 4)
+# 
+# try$eta_std_var
+# 
+# plot(1:length(try$eta_std_var), try$eta_std_var)
+# fpca_fit$diag.var
+# dim(fpca_fit$VarMats[[2]])
