ed55c4e88358cbf486534c15b733ad412c4bc057
Ying Jin <ying.jin@cuanschutz.edu>
2023-01-10 10:02:14 -0700

    back up before change device

--- Code/out_of_samp_bayes.R
+++ Code/out_of_samp_bayes.R
@@ -1,13 +1,10 @@
-
+library(cubature)
 # This script writes functions for 
 # calculating conditional expectation
 # of fPCA scores 
 # of an out-of-sample observation
 
 # first, only subject one, time up to 395
-rm(df_out)
-df_out1 <- df %>% filter(id == 1 & sind_inx <= 395) %>% select(-eta_i)
-max_bin <- which(mid==395)
 
 #### Denominator of E(xi|Y) ####
 # xi: scores. The value to integrate over
@@ -20,8 +17,11 @@ max_bin <- which(mid==395)
 tao <- diag(fpca_fit$evalues)
 f0 <- fpca_fit$mu[1:max_bin]
 phi <- fpca_fit$efunctions[1:max_bin, ] 
-df_out <- df_out1
+df_out <- df %>% filter(id == 1 & sind_inx <= 395) %>% select(-eta_i)
+max_bin <- which(mid==395)
 xi <- fpca_fit$scores[1, ]
+xi <- matrix(xi, nrow = 4, ncol = 1)
+k <- 4
   
 joint_pdf <- function(xi, tao, f0, phi, df_out){
   
@@ -35,15 +35,17 @@ joint_pdf <- function(xi, tao, f0, phi, df_out){
   eta = f0+phi%*%xi
   pr = exp(eta)/(1+exp(eta))
   # the part relevant to P(Y|xi)
-  prod((pr^hs)*(1-pr)^nf)*exp(-t(xi)%*%solve(tao)%*%xi/2)
-  # part relebant to marginal distribution of xi
+  p1 <- prod(dbinom(x=unlist(hs), size = ns, prob=pr))
+  p2 <- exp(-t(xi)%*%solve(tao)%*%xi/2)
   
-  
-  
+  return(p1*p2)
   
 }
 
-condi_pdf_y <- function(Y)
-  
+#test 
+joint_pdf(xi, tao, f0, phi, df_out)
+
+adaptIntegrate(joint_pdf, lower = rep(-500, K), upper = rep(500, K), 
+          tao=tao, f0=f0, phi=phi, df_out=df_out)
   
   
