31519515a33c719e2ae2e1a80273340df1312893
yingljin <ying.jin@cuanschutz.edu>
2023-01-11 12:06:22 -0700

    MLE scores and point prediction

--- Code/InSampPred.R
+++ Code/InSampPred.R
@@ -1,34 +1,32 @@
 
 ##### In-sample prediction on binned grid #####
 # fpca_fit: fpca model fit on estimated latent Gaussian function
-# tm: maximum observation time on binned grid, order index of bin
-# df_pred_unique: data frame with only id, bin, eta_hat
-# subj: subject ID to make prediction on
-# mid: label of binned grid
+# df_part: data frame including partial observations, with at least columns "bin", "eta_hat"
+
 
 # return: predictions on the full binned grid
 
-in_samp_dyn_pred <- function(fpca_fit, tm=20, df_pred_unique, subj=1){
+in_samp_dyn_pred <- function(fpca_fit, df_part){
   # values for prediction
-  max_t = unique(df_pred_unique$bin)[tm] # maximum observation time on bin label scale
+  tm <- length(unique(df_part$bin)) # maximum observation time on bin label scale
   tao <- diag(fpca_fit$evalues) ## egienvalues, also var-cov matrix of scores
   Phi <- fpca_fit$efunctions[1:tm, ] ## eigenfunctions on the entire binned grid
   
-  # observed prediction
-  partial_df <- df_pred_unique %>% filter(bin <= max_t & id == subj) 
   
   # re-estimate score
   H <-  tao %*% t(Phi) %*% solve(Phi%*%tao%*%t(Phi)+fpca_fit$sigma2*diag(tm))
-  score_hat <- H %*% (partial_df$eta_hat-fpca_fit$mu[1:tm])
+  score_hat <- H %*% (df_part$eta_hat-fpca_fit$mu[1:tm])
   
-  # predicte latent function
+  # predict latent function
   eta_pred <- fpca_fit$mu+fpca_fit$efunctions%*%score_hat
   
   # predict std
   eta_pred_var <- fpca_fit$efunctions %*% (tao - H %*% Phi %*% tao) %*% t(fpca_fit$efunctions) + fpca_fit$sigma2
   
-  return(list(eta_pred = eta_pred, eta_pred_var = eta_pred_var,
-              eta_std_var = sqrt(diag(eta_pred_var))))
+  return(list(eta_pred = eta_pred, 
+              eta_pred_var = eta_pred_var,
+              eta_std_var = sqrt(diag(eta_pred_var)),
+              score_hat = score_hat))
 }
 
 
---
+++ Code/OutSampMLE.R
@@ -0,0 +1,57 @@
+
+library(rootSolve)
+
+# This script writes functions for 
+# calculating MLE solution of fPCA scores 
+# based on conditonal distribution of score (conditional on observations)
+# of an out-of-sample observation
+# just for mess around
+
+
+#### MLE of out-of-sample score ####
+
+# First derivative of Log-likelihood
+# df_new: new observations, including at least columns Y and bin
+# fpca_fit: fpca object
+
+
+llh_div <- function(xi, df_new, fpca_fit){
+  
+  # observations
+  ns <- as.vector(table(df_new$bin)) # number of observations
+  hs <- df_new %>% group_by(bin) %>% summarize_at("Y", sum) %>% 
+    select(Y) %>% unlist()# number of success
+  nf <- ns-hs # number of failure
+  max_bin <- length(unique(df_new$bin)) # assume new skipped bins 
+  
+  # fpca objects
+  phi_m <- fpca_fit$efunctions[1:max_bin, ]
+  f0_m <- fpca_fit$mu[1:max_bin]
+  eta_s <- f0_m + phi_m %*% xi
+  tao <- diag(fpca_fit$evalues)
+  
+  # derivative
+  val1 <- hs-ns*(exp(eta_s)/(1+exp(eta_s)))
+  div <- colSums(apply(phi_m, 2, function(x)x*val1))-xi%*%solve(tao)
+  return(div)
+}
+
+# test
+# llh_div(rep(0, 4), df_new=df_new, fpca_fit = fpca_fit)
+
+#### Prediction with MLE scores #####
+
+out_samp_dyn_pred <- function(df_new, fpca_fit){
+  score_out <- multiroot(f=llh_div, start = rep(0, 4), df_new=df_new, fpca_fit=fpca_fit)$root
+  eta_pred_out <- fpca_fit$mu+fpca_fit$efunctions%*%score_out
+  
+  return(eta_pred_out)
+  
+}
+
+
+# test
+# out_samp_dyn_pred(df_new, fpca_fit)
+
+
+
