5f2031896cd2a057b065bc22d1566b2178c96ccf
yingljin <ying.jin@cuanschutz.edu>
2023-01-25 11:44:41 -0700

    try laplace demon package

---
+++ Code/OutSampBayes.R
@@ -0,0 +1,121 @@
+library(cubature)
+# This script writes functions for 
+# calculating conditional expectation
+# of fPCA scores 
+# of an out-of-sample observation
+
+# first, only subject one, time up to 395
+
+#### Numeric integration: denominator of E(xi|Y) ####
+# xi: scores. The value to integrate over
+# tao: estimated covariance matrix(eigenvalues) of score from fPCA
+# f0: mean function from fPCA (up to mth bin)
+# phi: basis function of fPCA (up to mth bin)
+# Y: newly observed data (up to mth bin)
+
+# just for mess around
+tao <- diag(fpca_fit$evalues)
+f0 <- fpca_fit$mu[1:max_bin]
+phi <- ç
+df_out <- df %>% filter(id == 1 & sind_inx <= 395) %>% select(-eta_i)
+max_bin <- which(mid==395)
+xi <- fpca_fit$scores[1, ]
+xi <- matrix(xi, nrow = 4, ncol = 1)
+k <- 4
+  
+joint_pdf <- function(xi, tao, f0, phi, df_out){
+  
+  # numbers
+  ns <- table(df_out$bin) # number of observations
+  hs <- df_out %>% group_by(bin) %>% summarize_at("Y", sum) %>% 
+    select(Y) # number of success
+  nf <- ns-hs # number of failure
+  
+  # probability of conditional distribution of Y
+  eta = f0+phi%*%xi
+  pr = exp(eta)/(1+exp(eta))
+  # the part relevant to P(Y|xi)
+  p1 <- prod(dbinom(x=unlist(hs), size = ns, prob=pr))
+  p2 <- exp(-t(xi)%*%solve(tao)%*%xi/2)
+  
+  return(p1*p2)
+  
+}
+
+#test 
+joint_pdf(xi, tao, f0, phi, df_out)
+
+adaptIntegrate(joint_pdf, lower = rep(-500, K), upper = rep(500, K), 
+          tao=tao, f0=f0, phi=phi, df_out=df_out)
+  
+# takes a really long time to run!
+
+#### Laplace approximation ####
+
+library(LaplacesDemon)
+library(mvtnorm)
+
+# experiment with one subject
+
+# data
+df_new <- df %>% filter(id==1 & sind_inx<=195) %>% select(-eta_i)
+ns <- as.vector(table(df_new$bin)) # number of observations
+hs <- df_new %>% group_by(bin) %>% summarize_at("Y", sum) %>% 
+  select(Y) %>% unlist()# number of success
+nf <- ns-hs # number of failure
+max_bin <- length(unique(df_new$bin)) # assume new skipped bins  
+
+df_new2 <- data.frame(bin = 1:max_bin, ns, hs, nf)
+
+## format into a list
+tao <- diag(fpca_fit$evalues)
+f0 <- fpca_fit$mu[1:max_bin]
+N <- nrow(df_new2) # "sample size" which is in fact number of observed bins in this case for a new subject
+n <- df_new2$ns # number of experiements at each bin
+y <- df_new2$hs # outcome, number of 1
+X <- fpca_fit$efunctions[1:max_bin, ] # "covariates", in this case PC functions
+J <- K # number of parameters/scores
+mon.names <- "LP"
+parm.names <- as.parm.names(list(xi1=0, xi2=0, xi3=0, xi4=0))
+pos.xi <- grep("xi", parm.names)
+PGF <- function(Data) {
+  xi <- rnorm(Data$J)
+  return(xi)
+}
+
+MyData <- list(J=J, PGF=PGF, X=X, mon.names=mon.names,
+              parm.names=parm.names, pos.xi=pos.xi, y=y, n=n, tao=tao, f0=f0)
+
+
+## model
+## additional values needed from FPCA
+
+
+Model <- function(parm, Data){
+  xi <- parm[Data$pos.xi]
+  
+  # log-prior
+  xi.prior <- dmvnorm(xi, mean = rep(0, Data$J), sigma=Data$tao, log = TRUE)
+  
+  # log-posterior likelihood
+  eta <- Data$f0+Data$X %*% xi
+  p <- exp(eta)/(1+exp(eta))
+  LL <- sum(dbinom(x=Data$y, size = Data$n, prob=p))
+  LP <- LL+sum(xi.prior)
+  
+  # output
+  Modelout <- list(LP=LP, Dev=-2*LL, Monitor=LL,
+                   yhat=Data$y, parm=parm)
+  return(Modelout)
+}
+
+# fit model
+Fit <- LaplacesDemon(Model, Data=MyData, rep(0, 4),
+                     Covar=NULL, Iterations=1000, Status=100, Thinning=1,
+                     Algorithm="AFSS", Specs=list(A=500, B=NULL, m=100, n=0, w=1))
+
+Fit2 <- LaplaceApproximation(Model, parm = rep(0, 4), Data=MyData)
+
+
+Consort(Fit)
+Fit2
---
+++ Scripts/LaplacesDemon.R
@@ -0,0 +1,3 @@
+library(LaplacesDemon)
+
+data("demonsnacks")
