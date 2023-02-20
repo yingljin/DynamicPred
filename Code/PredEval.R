cc3417abbed3a383bfad346a3b4aee12098dabe8
yingljin <ying.jin@cuanschutz.edu>
2023-02-20 12:53:28 -0700

    try evaluation at mid points

--- Code/PredEval.R
+++ Code/PredEval.R
@@ -9,10 +9,15 @@ eta_true_mat <- df %>%
   select(id, sind_inx, eta_i) %>%
   pivot_wider(names_from = id, values_from = eta_i) %>%
   column_to_rownames("sind_inx")
+
+# View(eta_true_mat)
+
 y_mat <- df %>%
   select(id, sind_inx, Y) %>%
   pivot_wider(names_from = id, values_from = Y) %>%
   column_to_rownames("sind_inx")
+
+# View(y_mat)
 
 ##### Observations up to 195 #####
 pred_t <- 196:max(mid)
@@ -26,18 +31,13 @@ for(i in 1:N){
   interp_pred <- approx(x=mid, y=pred, xout = pred_t)$y
   eta_hat_mat[, i] <- interp_pred
 }
+
 # ISE
 ise_t195 <- mean(apply((eta_hat_mat-eta_true_mat[pred_t, ])^2, 2, sum))
 # AUC
 prob_mat <- exp(eta_hat_mat)/(1+exp(eta_hat_mat))
 auc_perf <- performance(prediction(prob_mat, y_mat[pred_t, ]), "auc")
 mean(unlist(auc_perf@y.values))
-
-for(i in 1:5){
-  auc_perf <- performance(prediction(prob_mat[, i], y_mat[pred_t, i]), "auc")
-  print(auc_perf@y.values[[1]])
-}
-summary(auc_perf)
 
 
 
@@ -62,18 +62,26 @@ glmm_fit <- glmer(Y ~ sind_inx + (id | sind_inx),
 # marginal constrants? 
 fit_gam <- gam(Y ~ s(sind_inx, k=20, bs = "cr") + ti(id, sind_inx, bs=c("re","cr"), mc=c(TRUE,FALSE), k=c(5,5)), 
               family = "binomial",
-              data =  df %>% select(id, Y, sind_inx))
+              data =  df %>% select(id, Y, sind_inx) %>% filter(id %in% 1:100))
 
 
 
 ##### Midpoint ISE ##### 
 
-# not include AUC because it is difficult to determin "midpoint"
+# true eta and true y
+head(df_true_eta)
+table(df_true_eta$id)
+unique(df_true_eta$mid)
 
-# only true latent functions at bin midpoints are used
-eta_true_mat <- df_true_eta %>% pivot_wider(names_from = id, values_from = true) %>%
+eta_true_mat <- df_true_eta %>%
+  pivot_wider(names_from = id, values_from = true) %>%
   column_to_rownames("mid")
-eta_true_mat <- as.matrix(eta_true_mat)
+
+y_mat <- df %>%
+  select(id, sind_inx, Y) %>% 
+  filter(sind_inx %in% mid) %>% 
+  pivot_wider(names_from = id, values_from = Y) %>%
+  column_to_rownames("sind_inx")
 
 # observation up to 195
 eta_hat_mat <- matrix(NA, nrow=length(mid), ncol = N) # prediction container
@@ -82,39 +90,26 @@ for(i in 1:N){
                       select(-eta_i),
                     fpca_fit = fpca_fit)$eta_pred
 }
+
 ## do not include observed bins
-pred_rows <- which(mid > 195) # unobserved bin index
-eta_hat_mat[pred_rows,]
+pred_t <- which(mid > 195) # unobserved bin index
+
 ## ISE
-ise_t195 <- mean(apply((eta_hat_mat[pred_rows, ]-eta_true_mat[pred_rows, ])^2, 2, sum))
+ise_t195 <- mean(apply((eta_hat_mat[pred_t, ]-eta_true_mat[pred_t, ])^2, 2, sum))
 
-# observation up to 395
-eta_hat_mat <- matrix(NA, nrow=length(mid), ncol = N) # prediction container
-for(i in 1:N){
-  eta_hat_mat[ , i] <- out_samp_dyn_pred(df_new = df %>% filter(id==i & sind_inx<=395) %>%
-                                           select(-eta_i),
-                                         fpca_fit = fpca_fit)$eta_pred
-}
-## do not include observed bins
-pred_rows <- which(mid > 395) # unobserved bin index
-eta_hat_mat[pred_rows,]
-ise_t395 <- mean(apply((eta_hat_mat[pred_rows, ]-eta_true_mat[pred_rows, ])^2, 2, sum))
+## AUC
+prob_mat <- exp(eta_hat_mat)/(1+exp(eta_hat_mat))
+auc_perf <- performance(prediction(prob_mat[pred_t, ], y_mat[pred_t, ]), "auc")
+mean(unlist(auc_perf@y.values))
 
-# observation up to 595
-eta_hat_mat <- matrix(NA, nrow=length(mid), ncol = N) # prediction container
-for(i in 1:N){
-  eta_hat_mat[ , i] <- out_samp_dyn_pred(df_new = df %>% filter(id==i & sind_inx<=595) %>%
-                                           select(-eta_i),
-                                         fpca_fit = fpca_fit)$eta_pred
-}
-## do not include observed bins
-pred_rows <- which(mid > 595) # unobserved bin index
-eta_hat_mat[pred_rows,]
-ise_t395 <- mean(apply((eta_hat_mat[pred_rows, ]-eta_true_mat[pred_rows, ])^2, 2, sum))
-
-
-
-
+## Reference model: 
+### functional mixed models/generalized function-on-scalar regression
+# marginal constrants? 
+fit_gam <- gam(Y ~ s(sind_inx, k=20, bs = "cr") + ti(id, sind_inx, bs=c("re","cr"), mc=c(TRUE,FALSE), k=c(5,5)), 
+               family = "binomial",
+               data =  df %>%
+                 select(id, sind_inx, Y) %>% 
+                 filter(sind_inx %in% mid))
 
 
 
