c245b4f0bf7693533301913ced01ff63270b1d9e
yingljin <ying.jin@cuanschutz.edu>
2023-02-24 12:35:07 -0700

    prediction performance evaluation

--- Code/PredEval.R
+++ Code/PredEval.R
@@ -1,136 +1,101 @@
 # This script codes the computaiton of evaluation of predictive performance
 
 library(ROCR)
-
-##### Interpolated ISE and AUC #####
-
-# true eta and true y
-eta_true_mat <- df %>%
-  select(id, sind_inx, eta_i) %>%
-  pivot_wider(names_from = id, values_from = eta_i) %>%
-  column_to_rownames("sind_inx")
-
-# View(eta_true_mat)
-
-y_mat <- df %>%
-  select(id, sind_inx, Y) %>%
-  pivot_wider(names_from = id, values_from = Y) %>%
-  column_to_rownames("sind_inx")
-
-# View(y_mat)
-
-##### Observations up to 195 #####
-pred_t <- 196:max(mid)
-eta_hat_mat <- matrix(NA, nrow=length(pred_t), ncol = N) # prediction container
-dim(eta_hat_mat)
-for(i in 1:N){
-  pred <- out_samp_dyn_pred(df_new = df %>% 
-                              filter(id==i & sind_inx<=195) %>%
-                              select(-eta_i),
-                            fpca_fit = fpca_fit)$eta_pred
-  interp_pred <- approx(x=mid, y=pred, xout = pred_t)$y
-  eta_hat_mat[, i] <- interp_pred
-}
-
-# ISE
-ise_t195 <- mean(apply((eta_hat_mat-eta_true_mat[pred_t, ])^2, 2, sum))
-# AUC
-prob_mat <- exp(eta_hat_mat)/(1+exp(eta_hat_mat))
-auc_perf <- performance(prediction(prob_mat, y_mat[pred_t, ]), "auc")
-mean(unlist(auc_perf@y.values))
-
-
-
-##### Reference methods #####
-
-# binary FPCA for dynamic prediction
-# non-convergence
-library(registr)
-
-bfpca_fit <- bfpca(Y= df %>% select(id, Y, sind_inx) %>% rename(index=sind_inx, value = Y), 
-      npc_varExplained = 0.95, t_min=1, t_max = J)
-
-# A standard mxied model
-# time as the only fixed effect
-# random intercept for ID and random slope for time
-# slow, also with numeric issues
-glmm_fit <- glmer(Y ~ sind_inx + (id | sind_inx),
-                  data = df %>% select(id, Y, sind_inx), family = binomial)
-
-
-# Adaptive GLMM
-
 library(GLMMadaptive)
 
-View(df)
-fm1 <- mixed_model(Y ~ sind, random = ~ sind | id, 
-                   data =  df,
-                   family = binomial())
 
-preds_fm1 <- predict(fm1, 
-                     newdata = df %>% filter(sind_inx <= 195),
-                     newdata2 =  df %>% filter(sind_inx > 195), 
-                     type = "subject_specific", type_pred = "link",
-                     se.fit = TRUE, return_newdata = TRUE)
-head(preds_fm1$newdata2)
+obs_t <- c(195, 395, 595)
 
-ggplot(preds_fm1$newdata2%>% filter(id==1))+
-  geom_line(aes(x=sind_inx, y=pred))
-#large coefficient issue
+ise_fgfpca <- rep(NA, 3)
+ise_adglmm <- rep(NA, 3)
 
-# functional mixed models
-# generalized function-on-scalar regression
-# marginal constrants? 
-fit_gam <- gam(Y ~ s(sind_inx, k=20, bs = "cr") + ti(id, sind_inx, bs=c("re","cr"), mc=c(TRUE,FALSE), k=c(5,5)), 
-              family = "binomial",
-              data =  df %>% select(id, Y, sind_inx) %>% filter(id %in% 1:100))
+auc_fgfpca <- rep(NA, 3)
+auc_adglmm <- rep(NA, 3)
 
+comp_t_fgfpca <- rep(NA, 3)
+comp_t_adglmm <- rep(NA, 3)
 
+adglmm <- mixed_model(Y ~ sind, random = ~ sind | id, data =  df,
+                      family = binomial())
 
 ##### Midpoint ISE ##### 
 
 # true eta and true y
-head(df_true_eta)
-table(df_true_eta$id)
-unique(df_true_eta$mid)
+# head(df_true_eta)
+# table(df_true_eta$id)
+# unique(df_true_eta$mid)
 
 eta_true_mat <- df_true_eta %>%
   pivot_wider(names_from = id, values_from = true) %>%
   column_to_rownames("mid")
 
-y_mat <- df %>%
-  select(id, sind_inx, Y) %>% 
-  filter(sind_inx %in% mid) %>% 
+y_mat <- df %>% select(id, Y, sind_inx) %>% 
   pivot_wider(names_from = id, values_from = Y) %>%
   column_to_rownames("sind_inx")
 
 # observation up to 195
-eta_hat_mat <- matrix(NA, nrow=length(mid), ncol = N) # prediction container
-for(i in 1:N){
-  eta_hat_mat[ , i] <- out_samp_dyn_pred(df_new = df %>% filter(id==i & sind_inx<=195) %>%
-                      select(-eta_i),
-                    fpca_fit = fpca_fit)$eta_pred
+
+for(j in 1: length(obs_t)){
+  
+  t <- obs_t[j]
+  pred_t <- which(mid > t) # unobserved bin index (binned grid)
+  pred_t_full <- (t+1):max(mid) # unobserved time index (original grid)
+  
+  # fast GFPCA prediction container
+  eta_hat_mat <- matrix(NA, nrow=length(mid), ncol = N)
+  eta_hat_mat_full <- matrix(NA, nrow=J, ncol = N) # latent Gaussian function on original grid by interpolation
+ 
+  # prediction
+  tstart <- Sys.time()
+  for(i in 1:N){
+    eta_hat_mat[ , i] <- out_samp_dyn_pred(df_new = df %>% filter(id==i & sind_inx<=t) %>%
+                                             select(-eta_i),
+                                           fpca_fit = fpca_fit)$eta_pred
+    eta_hat_mat_full[, i] <- approx(x=mid, y=eta_hat_mat[ , i], xout = 1:J)$y
+  }
+  tend <- Sys.time()
+  
+  ## ISE
+  ise_fgfpca[j] <- mean(apply((eta_hat_mat[pred_t, ]-eta_true_mat[pred_t, ])^2, 2, sum))
+  ## AUC after interpolation
+  prob_mat <- exp(eta_hat_mat_full[pred_t_full, ])/(1+exp(eta_hat_mat_full[pred_t_full, ]))
+  auc_perf <- performance(prediction(prob_mat, y_mat[pred_t_full,]), "auc")
+  auc_fgfpca[j] <-mean(unlist(auc_perf@y.values))
+  ## computation time
+  comp_t_fgfpca[j] <- tend-tstart
+  
+  # reference method: adaptive GLMM
+  # https://drizopoulos.github.io/GLMMadaptive/
+  # fit model with full data (time as fixed effect, random intercept and slopt for time)
+  # only evaluate SE at bin midpoint
+  tstart2 <- Sys.time()
+  preds_adglmm <- predict(adglmm, 
+                       newdata = df %>% filter(sind_inx <= t),
+                       newdata2 =  df %>% filter(sind_inx > t), 
+                       type = "subject_specific", type_pred = "link",
+                       se.fit = TRUE, return_newdata = TRUE)
+  tend2 <- Sys.time()
+  
+  eta_hat_mat2 <- preds_adglmm$newdata2 %>% select(id, sind_inx, pred) %>% 
+    filter(sind_inx %in% mid) %>%
+    pivot_wider(names_from = id, values_from = pred) %>% 
+    column_to_rownames("sind_inx") %>% as.matrix()
+  eta_hat_mat_full2 <- preds_adglmm$newdata2 %>% select(id, sind_inx, pred) %>% 
+    filter(sind_inx <= max(mid)) %>% 
+    pivot_wider(names_from = id, values_from = pred) %>% 
+    column_to_rownames("sind_inx") %>% as.matrix()
+
+ ## ISE
+ ise_adglmm[j] <- mean(apply((eta_hat_mat2-eta_true_mat[pred_t, ])^2, 2, sum)) 
+ ## AUC after interpolation
+ prob_mat2 <- exp(eta_hat_mat_full2)/(1+exp(eta_hat_mat_full2))
+ auc_perf2 <- performance(prediction(prob_mat2, y_mat[pred_t_full, ]), "auc")
+ auc_adglmm[j] <-mean(unlist(auc_perf2@y.values))
+ # computation time
+ comp_t_adglmm[j] <- tend2-tstart2
+
 }
 
-## do not include observed bins
-pred_t <- which(mid > 195) # unobserved bin index
-
-## ISE
-ise_t195 <- mean(apply((eta_hat_mat[pred_t, ]-eta_true_mat[pred_t, ])^2, 2, sum))
-
-## AUC
-prob_mat <- exp(eta_hat_mat)/(1+exp(eta_hat_mat))
-auc_perf <- performance(prediction(prob_mat[pred_t, ], y_mat[pred_t, ]), "auc")
-mean(unlist(auc_perf@y.values))
-
-## Reference model: 
-### functional mixed models/generalized function-on-scalar regression
-# marginal constrants? 
-fit_gam <- gam(Y ~ s(sind_inx, k=20, bs = "cr") + ti(id, sind_inx, bs=c("re","cr"), mc=c(TRUE,FALSE), k=c(5,5)), 
-               family = "binomial",
-               data =  df %>%
-                 select(id, sind_inx, Y) %>% 
-                 filter(sind_inx %in% mid))
 
 
 
