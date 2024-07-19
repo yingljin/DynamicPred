d07e853cf0fcffda1af362c88a9176037014d4cd
yingljin <ying.jin@cuanschutz.edu>
2024-07-19 11:02:15 -0600

    figures with prediction interval

--- Code/PredEval.R
+++ Code/PredEval.R
@@ -1,100 +1,107 @@
-# This script codes the computaiton of evaluation of predictive performance
+# This script codes the functions of evaluation of predictive performance
 
-library(ROCR)
-library(GLMMadaptive)
+#### A function to calculate ISE ####
 
+## calculation
+# pred list: must contain columns: pred.., eta_i, id
 
-obs_t <- c(195, 395, 595)
-
-ise_fgfpca <- rep(NA, 3)
-ise_adglmm <- rep(NA, 3)
-
-auc_fgfpca <- rep(NA, 3)
-auc_adglmm <- rep(NA, 3)
-
-comp_t_fgfpca <- rep(NA, 3)
-comp_t_adglmm <- rep(NA, 3)
-
-adglmm <- mixed_model(Y ~ sind, random = ~ sind | id, data =  df,
-                      family = binomial())
-
-##### Midpoint ISE ##### 
-
-# true eta and true y
-# head(df_true_eta)
-# table(df_true_eta$id)
-# unique(df_true_eta$mid)
-
-eta_true_mat <- df_true_eta %>%
-  pivot_wider(names_from = id, values_from = true) %>%
-  column_to_rownames("mid")
-
-y_mat <- df %>% select(id, Y, sind_inx) %>% 
-  pivot_wider(names_from = id, values_from = Y) %>%
-  column_to_rownames("sind_inx")
-
-# observation up to 195
-
-for(j in 1: length(obs_t)){
+calc_ISE <- function(pred_list, window){
   
-  t <- obs_t[j]
-  pred_t <- which(mid > t) # unobserved bin index (binned grid)
-  pred_t_full <- (t+1):max(mid) # unobserved time index (original grid)
+  M <- length(pred_list)  # number of simulations
   
-  # fast GFPCA prediction container
-  eta_hat_mat <- matrix(NA, nrow=length(mid), ncol = N)
-  eta_hat_mat_full <- matrix(NA, nrow=J, ncol = N) # latent Gaussian function on original grid by interpolation
- 
-  # prediction
-  tstart <- proc.time()
-  for(i in 1:N){
-    eta_hat_mat[ , i] <- out_samp_dyn_pred(df_new = df %>% filter(id==i & sind_inx<=t) %>%
-                                             select(-eta_i),
-                                           fpca_fit = fpca_fit)$eta_pred
-    eta_hat_mat_full[, i] <- approx(x=mid, y=eta_hat_mat[ , i], xout = 1:J)$y
+  # container
+  ise_arr <- array(NA, dim = c(length(window)-1, length(window)-2, M))
+  # prediction window by max obervation time by iteration
+  
+  # within simulation, average over subject
+  for(m in 1:M){
+    ise_tb <- pred_list[[m]] %>%
+      mutate(err1 = (pred0.2-eta_i)^2,
+             err2 = (pred0.4-eta_i)^2,
+             err3 = (pred0.6-eta_i)^2,
+             err4 = (pred0.8-eta_i)^2) %>%
+      select(id, t, starts_with("err")) %>% 
+      mutate(window = cut(t, breaks = window, include.lowest = T)) %>% 
+      group_by(window, id) %>% 
+      summarise_at(vars(err1, err2, err3, err4), sum) %>% 
+      group_by(window) %>% 
+      summarize_at(vars(err1, err2, err3, err4), mean) %>%
+      # filter(window != "[0,0.2]") %>% 
+      select(starts_with("err"))
+    
+    ise_arr[ , , m] <- as.matrix(ise_tb)
+   
   }
-  # tend <- Sys.time()
-  comp_t_fgfpca[j] <- (proc.time()-tstart)[3]
-  
-  ## ISE
-  ise_fgfpca[j] <- mean(apply((eta_hat_mat[pred_t, ]-eta_true_mat[pred_t, ])^2, 2, sum))
-  ## AUC after interpolation
-  prob_mat <- exp(eta_hat_mat_full[pred_t_full, ])/(1+exp(eta_hat_mat_full[pred_t_full, ]))
-  auc_perf <- performance(prediction(prob_mat, y_mat[pred_t_full,]), "auc")
-  auc_fgfpca[j] <-mean(unlist(auc_perf@y.values))
-  
-  # reference method: adaptive GLMM
-  # https://drizopoulos.github.io/GLMMadaptive/
-  # fit model with full data (time as fixed effect, random intercept and slopt for time)
-  # only evaluate SE at bin midpoint
-  tstart2 <- proc.time()
-  preds_adglmm <- predict(adglmm, 
-                       newdata = df %>% filter(sind_inx <= t),
-                       newdata2 =  df %>% filter(sind_inx > t), 
-                       type = "subject_specific", type_pred = "link",
-                       se.fit = TRUE, return_newdata = TRUE)
-  # tend2 <- Sys.time()
-  # computation time
-  comp_t_adglmm[j] <- (proc.time()-tstart2)[3]
-  
-  eta_hat_mat2 <- preds_adglmm$newdata2 %>% select(id, sind_inx, pred) %>% 
-    filter(sind_inx %in% mid) %>%
-    pivot_wider(names_from = id, values_from = pred) %>% 
-    column_to_rownames("sind_inx") %>% as.matrix()
-  eta_hat_mat_full2 <- preds_adglmm$newdata2 %>% select(id, sind_inx, pred) %>% 
-    filter(sind_inx <= max(mid)) %>% 
-    pivot_wider(names_from = id, values_from = pred) %>% 
-    column_to_rownames("sind_inx") %>% as.matrix()
 
- ## ISE
- ise_adglmm[j] <- mean(apply((eta_hat_mat2-eta_true_mat[pred_t, ])^2, 2, sum)) 
- ## AUC after interpolation
- prob_mat2 <- exp(eta_hat_mat_full2)/(1+exp(eta_hat_mat_full2))
- auc_perf2 <- performance(prediction(prob_mat2, y_mat[pred_t_full, ]), "auc")
- auc_adglmm[j] <-mean(unlist(auc_perf2@y.values))
+  # average over simulation
+  ise_arr <- apply(ise_arr, c(1, 2), mean, na.rm = T)
+  colnames(ise_arr) <- c("0.2", "0.4", "0.6", "0.8")
+  
+  return(ise_arr)
+}
 
+
+#### Functions for AUC ####
+
+## a function to calculate AUC with NA presence
+get_auc <- function(y, pred){
+  if(sum(is.na(y))>0 | sum(is.na(pred))>0){
+    auc <- NA
+  }
+  else{
+    this_perf <- performance(prediction(pred, y), measure = "auc")
+    auc <- this_perf@y.values[[1]]
+  }
+  return(auc)
+}
+
+
+## calculate AUC for simulation
+
+calc_AUC <- function(pred_list, window){
+  
+  M <- length(pred_list)  # number of simulations
+  
+  # container
+  auc_arr <- array(NA, dim = c(length(window)-1, length(window)-2, M))
+  # prediction window by max obervation time by iteration
+  
+  # calculate AUC over subjects within one simulation
+  for(m in 1:M){
+    auc_tb <- pred_list[[m]] %>% 
+      mutate(window = cut(t, breaks = window, include.lowest = T)) %>% 
+      select(Y, starts_with("pred"), window) %>%
+      group_by(window) %>%
+      summarise(auc1 = get_auc(Y, pred0.2),
+                auc2 = get_auc(Y, pred0.4),
+                auc3 = get_auc(Y, pred0.6),
+                auc4 = get_auc(Y, pred0.8)) %>%
+      select(starts_with("auc"))
+    auc_arr[, ,m] <- as.matrix(auc_tb)
+  }
+  
+  # average over simulation
+  auc_arr <- apply(auc_arr, c(1, 2), mean, na.rm = T)
+  colnames(auc_arr) <- c("0.2", "0.4", "0.6", "0.8")
+  
+  return(auc_arr)
 }
 
 
 
+#### Interval coverage rate ####
+calc_predint_cover <- function(pred_list){
+  
+  covrate <- pred_list %>% bind_rows(.id="sim") %>%
+    mutate(cover0.2 = pred0.2_lb<=eta_i & pred0.2_ub>=eta_i,
+           cover0.4 = pred0.4_lb<=eta_i & pred0.4_ub>=eta_i,
+           cover0.6 = pred0.6_lb<=eta_i & pred0.6_ub>=eta_i,
+           cover0.8 = pred0.8_lb<=eta_i & pred0.8_ub>=eta_i) %>% 
+    group_by(sim, t) %>%
+    summarize_at(vars(starts_with("cover")), mean) %>% 
+    group_by(t) %>%
+    summarize_at(vars(starts_with("cover")), mean)
+  
+  return(covrate)
+}
 
